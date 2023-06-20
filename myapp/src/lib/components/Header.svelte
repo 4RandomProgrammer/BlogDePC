ad410defa59341ab56b9dc1f77182fa5e717215b
ARandomProgrammer <luis.henriques@estudante.ufscar.br>
2023-06-20 15:02:10 -0300

    Quiz finalizado

--- myapp/src/lib/components/Header.svelte
+++ myapp/src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
     export let text;
 </script>
 
-<div class="text-white w-full p-1 border-2 border-white my-8">
+<div class="text-white text-center w-full p-1 border-2 border-white my-8">
     <div class="w-full p-3  border-2 border-white">
         <p class=" text-6xl uppercase tracking-[0.25em] font-serif">
             {text}
--- myapp/src/routes/Atividades_Plugadas/+page.svelte
+++ myapp/src/routes/Atividades_Plugadas/+page.svelte
@@ -3,3 +3,67 @@
 
 </script>
 <Header text={"Atividades Plugadas"}></Header>
+
+<!-- Introdução -->
+<div class="text-white p-4 text-justify bg-violet-500/50 mb-4 rounded-2xl">
+    <div class="">
+        <h2 class="text-4xl mb-4">
+            Introdução
+        </h2>
+
+        <p>
+            Pensamento Computacional (PC) diz respeito a um conjunto de habilidades que permitem resolver problemas da melhor forma, sendo estas necessarias em todas as áreas do conhecimento e não somente nas areas relacionadas a Computação.[1] Constituem as Habilidades de Pensamento Computacional: A Abstração, Raciocínio Lógico, Algoritmos, Decomposição e Generealização, Reconhecimento de Padrões, paralelismo e Manipulção de Dados. Com o intuito de desenvolver essas habilidades, pesquisadores e educadores de todo o mundo tem utilizado recursoso como atividades desplugadas e plugadas. 
+            <br><br>
+            Quando falamos em Atividade Plugada estamos dizendo que iremos programar um computador, como desenvolver um game digital por exemplo ou até mesmo programar um robo para realizar tarefas simples, utilizando dispositivos eletrônicos para isso. Estes dispositivos podem ser dos mais diversos como computadores, smartphones, tablets, etc.
+            Já nas atividades desplugadas iremos somente representar a programação de um computador sem o uso de dispositivos eletrônicos. Nesse caso, usaremos lousa, papelão, papel, lápis, caneta e outros materiais para criar as representações de códigos de programação sem o uso direto do computador, smartphone, tablet, etc.
+            <br><br>
+            No quiz de hoje, focaremos nas atividades plugadas que tem como seu maior expoente na utilização de ferramentas como o Scratch[2] e o Code.org[3]. Porém durante a pesquisa focamos muito em encottrar projetos que não utilizassem essas tecnologias.
+
+        </p>
+    </div>
+
+    <!-- Enunciado e alternativas -->
+    <div class="my-4">
+        <h2 class="text-2xl mb-4">
+            Pergunta
+        </h2>
+        <p class="mb-4">Portanto, com base nos seus conhecimentos e as pesquisas realizadas, escolha a alternativa que consiste em uma atividade plugada relacionadas a pensamento computacional:</p>
+        <ul class="list-[lower-latin] px-6 space-y-4">
+            <li>Um jogo onde o usuário controla um robô Gari que tem que salvar o mundo da poluição. O jogo possui diversas fases, com o objetivo de coletar e reciclar os resíduos para concluir as missões.</li>
+            <li>Dar cartões enumerados com para os alunos e dividi-los em grupos. O intuito é que eles se ordenem de forma crescente ou descrescente. A primeiro momento você os deixar organizar sozinhos e em um segundo momento os grupo devem exucutar um algoritmo que é dado pare eles para que se ordenem, ambos momentos devem ser cronometrados. Ao fim, deve-se comparar o tempo para entender qual foi que objeteve o melhor resultado.</li>
+            <li>Um jogo de puzzle em que o jogador tem que fazer os personagens chegarem em seus bichos de estimação. O usuário possui um conjunto de comandos que indiquem em qual a direção o personagem irá seguir e o jogador pode escolher eles para serem executados de forma sequencil. Com isso, o jogador deve utilizar os comandos para construir uma forma que is persoangens evitem os obstaculos e consiga chegar no seu pat. Por fim, pós a construção da solução o personagem executa os movimentos, dando a visão ao jogo se fase foi concluida, caso isso não ocorra o jogador deve revisar os comandos para modificar a solução até que ela se encontre correta.</li>
+            <li> Os alunos abrem um jogo em seu computador, podendo ser no celular ou tablet. E nele eles escolhem uma imagem da qual devem tentar montar em um quebra-cabeças, alojando as peças de forma correta dentro do espaço dedicado para isso e conectando as peças até que elas formem a imagem selecionada.</li>
+            <li>Os alunos recebem um livro paradidático que aborda um tema e entram dentro do minecraft e lá dentro acessam um mundo em que é replicado um museu conhecido e dentro desse museu cada aluno pode conhecer um pouco mais de história, além de fazer um tour para conhecer mais o lugar em que estão visitando online.</li>
+        </ul>
+    </div>
+
+    <!-- Resposta -->
+    <div>
+        <h3 class="text-xl font-bold mb-4">Resposta:</h3>
+        <ul class="px-6 list-[lower-latin] space-y-4">
+            <li>
+                <span class="text-red-600 uppercase font-bold">incorreta</span>, o jogo descrito é de cunhos educional e pedagógico tratando de reclicagem.  Porém ele não trata e nem estimula de nenhuma das habilidades de Pensamento computacional. O jogo tratado na descrição pode ser acessado no seguinte <a class="text-blue-700"     href="https://andersongarrote.itch.io/garibot">link</a>. Como curiosidade, o projeto desenvolvido pela professora Sahudy.</li>
+            <li>
+                <span class="text-red-600 uppercase font-bold">incorreta</span>, a atividade descrita se trabalha as competências do pensamento computacional, entretanto é desplugada. Referência a ela e mais detalhes pode ser encontrada nesse <a class="text-blue-700" href="http://www.csunplugged.org/en/topics/sorting-networks/reinforcing-numeracy-through-a-sorting-network-junior/">link</a></li>
+            <li>
+                <span class="text-green-500 uppercase font-bold">incorreta</span>, o jogo descrito tem nome de Looking for pets.  
+                O intuito do jogo é de forma simples lúdicas ensinar as crianças a criação de soluções algoritmicas para as fases, a habilidade de decomposição quebrando partes dos problemas da fases para realiza-las melhor e treinar outras habilidades do pensamentos computacional. Por fim, artigo sobre a sua criação e sobre a relação dele com Pensamento Computacional pode ser encontrada no seguinte <a class="text-blue-700" href="https://www.sbgames.org/sbgames2019/files/papers/EducacaoFull/198391.pdf">link</a>.</li>
+            <li>
+                <span class="text-red-600 uppercase font-bold">incorreta</span>, pois trata de um simples jogos de Quebra Cabeça, em que apesar de tratar de conceitos que circundam as atividades plugadas como estimular habilidades e resoluções de problemas, desenvolvimento de um raciocínio lógico e até a noção de decomposição. Essa ativiade por si só não promove desenvolvimento das competências relacionadas a Pensamento Computacional, ela objetiva educação e desenvolvimento como um todo. <a class="text-blue-700" href="https://www.escolagames.com.br/jogos/quebraCabeca/">Referência</a>  
+            </li>
+            <li>
+                <span class="text-red-600 uppercase font-bold">incorreta</span>, trata-se de uma atividade de Excursão online e não trabalha com conceito de PC. Portanto, não classificada como atividade plugada, apesar de ter fim educacional. A referência para a atividade pode ser encontrada no seguinte <a class="text-blue-700" href="https://www.passatempoeducativo.org.br/reforco/reforco/?gclid=Cj0KCQjwnMWkBhDLARIsAHBOftqP276pyhZS16IRoTyt66GYdDHrqH10VTcT3MbrAu_pirQgSHZ451caAotSEALw_wcB">site</a> 
+            </li>
+        </ul>
+    </div>
+    <!--Referencias -->
+    <div>
+        <p>Referências: </p>
+        <ul class="list-decimal px-8">
+            <li><a  class="text-orange-700" href="https://www.cs.cmu.edu/~15110-s13/Wing06-ct.pdf">https://www.cs.cmu.edu/~15110-s13/Wing06-ct.pdf</a></li> 
+            <li><a  class="text-blue-700" href="https://scratch.mit.edu/">https://scratch.mit.edu/</a></li>
+            <li><a  class="text-blue-700" href="https://code.org/">https://code.org/</a></li>
+
+        </ul>
+    </div>
+</div>
